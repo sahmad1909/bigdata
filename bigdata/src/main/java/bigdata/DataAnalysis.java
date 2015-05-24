c3af4071735c3ccb6cc0ccf99e6defab3277ef39
sahmad1909 <sikandarahmad@hotmail.com>
2015-05-24 10:58:53 -0700

    Added mean and mode

--- bigdata/src/main/java/bigdata/DataAnalysis.java
+++ bigdata/src/main/java/bigdata/DataAnalysis.java
@@ -2,6 +2,8 @@ package bigdata;
 
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -10,7 +12,6 @@ import org.json.simple.parser.JSONParser;
 public class DataAnalysis {
 
 	public static final String WEATHER_DELAY = "weatherdelay";
-	
 
 	static String[] flightAttributes = { "flightdate", "flightnum",
 			"airlineid", "carrier", "originairportid", "origincityname",
@@ -23,72 +24,151 @@ public class DataAnalysis {
 		JSONArray jsonArray = readJsonFile(path);
 
 		// Calculating Mean
-		System.out.println("Weather Mean = "+ calculateMean(jsonArray, WEATHER_DELAY));
+		System.out.println("Weather Mean = "
+				+ calculateMean(jsonArray, WEATHER_DELAY));
 
-		System.out.println("Arr Delay = " + calculateMean(jsonArray, "arrdelay"));		
-		
-		System.out.println("Late Aircract Delay = " + calculateMean(jsonArray, "lateaircraftdelay"));		
-		
-		System.out.println("Divarrdelay Delay = " + calculateMean(jsonArray, "divarrdelay"));		
-		
-		System.out.println("Depdelay Delay = " + calculateMean(jsonArray, "depdelay"));
-		
-		System.out.println("securitydelay Delay = " + calculateMean(jsonArray, "securitydelay"));
-		
-		System.out.println("NASdelay Delay = " + calculateMean(jsonArray, "nasdelay"));		
-		
-		System.out.println("carrierdelay Delay = " + calculateMean(jsonArray, "carrierdelay"));
-		
-		
-		// Standard Devitaion 
-		
-		System.out.println(" ********************************************************************************************************  ");
-		
-		Statistics statistics = new Statistics(getFlightData(jsonArray,WEATHER_DELAY));		
+		System.out.println("Arr Delay = "
+				+ calculateMean(jsonArray, "arrdelay"));
+
+		System.out.println("Late Aircract Delay = "
+				+ calculateMean(jsonArray, "lateaircraftdelay"));
+
+		System.out.println("Divarrdelay Delay = "
+				+ calculateMean(jsonArray, "divarrdelay"));
+
+		System.out.println("Depdelay Delay = "
+				+ calculateMean(jsonArray, "depdelay"));
+
+		System.out.println("securitydelay Delay = "
+				+ calculateMean(jsonArray, "securitydelay"));
+
+		System.out.println("NASdelay Delay = "
+				+ calculateMean(jsonArray, "nasdelay"));
+
+		System.out.println("carrierdelay Delay = "
+				+ calculateMean(jsonArray, "carrierdelay"));
+
+		// Standard Devitaion
+
+		System.out
+				.println(" ********************************************************************************************************  ");
+
+		Statistics statistics = new Statistics(getFlightData(jsonArray,
+				WEATHER_DELAY));
 		System.out.println("Weather Std Deviation : " + statistics.getStdDev());
+
+		statistics = new Statistics(getFlightData(jsonArray, "arrdelay"));
+		System.out
+				.println("arrdelay Std Deviation : " + statistics.getStdDev());
+
+		statistics = new Statistics(getFlightData(jsonArray,
+				"lateaircraftdelay"));
+		System.out.println("lateaircraftdelay Std Deviation : "
+				+ statistics.getStdDev());
+
+		statistics = new Statistics(getFlightData(jsonArray, "divarrdelay"));
+		System.out.println("divarrdelay Std Deviation : "
+				+ statistics.getStdDev());
+
+		statistics = new Statistics(getFlightData(jsonArray, "depdelay"));
+		System.out
+				.println("depdelay Std Deviation : " + statistics.getStdDev());
+
+		statistics = new Statistics(getFlightData(jsonArray, "securitydelay"));
+		System.out.println("securitydelay Std Deviation : "
+				+ statistics.getStdDev());
+
+		statistics = new Statistics(getFlightData(jsonArray, "nasdelay"));
+		System.out
+				.println("nasdelay Std Deviation : " + statistics.getStdDev());
+
+		statistics = new Statistics(getFlightData(jsonArray, "carrierdelay"));
+		System.out.println("carrierdelay Std Deviation : "
+				+ statistics.getStdDev());
 		
-		 statistics = new Statistics(getFlightData(jsonArray,"arrdelay"));		
-		System.out.println("arrdelay Std Deviation : " + statistics.getStdDev());
 		
 		
-		 statistics = new Statistics(getFlightData(jsonArray,"lateaircraftdelay"));		
-		System.out.println("lateaircraftdelay Std Deviation : " + statistics.getStdDev());
+
+		System.out
+				.println(" ********************************************************************************************************  ");
 		
-		 statistics = new Statistics(getFlightData(jsonArray,"divarrdelay"));		
-		System.out.println("divarrdelay Std Deviation : " + statistics.getStdDev());
+
+
 		
-		 statistics = new Statistics(getFlightData(jsonArray,"depdelay"));		
-		System.out.println("depdelay Std Deviation : " + statistics.getStdDev());
+		System.out.println("Weather Median = "
+				+ getMedian(jsonArray, WEATHER_DELAY));
+
+		System.out.println("Arr Delay = "
+				+ getMedian(jsonArray, "arrdelay"));
+
+		System.out.println("Late Aircract Delay = "
+				+ getMedian(jsonArray, "lateaircraftdelay"));
+
+		System.out.println("Divarrdelay Delay = "
+				+ getMedian(jsonArray, "divarrdelay"));
+
+		System.out.println("Depdelay Delay = "
+				+ getMedian(jsonArray, "depdelay"));
+
+		System.out.println("securitydelay Delay = "
+				+ getMedian(jsonArray, "securitydelay"));
+
+		System.out.println("NASdelay Delay = "
+				+ getMedian(jsonArray, "nasdelay"));
+
+		System.out.println("carrierdelay Delay = "
+				+ getMedian(jsonArray, "carrierdelay"));
 		
 		
-		 statistics = new Statistics(getFlightData(jsonArray,"securitydelay"));		
-		 System.out.println("securitydelay Std Deviation : " + statistics.getStdDev());
-			
-			
-		  statistics = new Statistics(getFlightData(jsonArray,"nasdelay"));		
-		  System.out.println("nasdelay Std Deviation : " + statistics.getStdDev());
-		  
-		  
-		  statistics = new Statistics(getFlightData(jsonArray,"carrierdelay"));		
-		  System.out.println("carrierdelay Std Deviation : " + statistics.getStdDev());
 		
 		
+		System.out
+		.println(" ********************************************************************************************************  ");
+		
+		
+		System.out.println("Weather Median = "
+				+ getMode(jsonArray, WEATHER_DELAY));
+
+		System.out.println("Arr Delay = "
+				+ getMode(jsonArray, "arrdelay"));
+
+		System.out.println("Late Aircract Delay = "
+				+ getMode(jsonArray, "lateaircraftdelay"));
+
+		System.out.println("Divarrdelay Delay = "
+				+ getMode(jsonArray, "divarrdelay"));
+
+		System.out.println("Depdelay Delay = "
+				+ getMode(jsonArray, "depdelay"));
+
+		System.out.println("securitydelay Delay = "
+				+ getMode(jsonArray, "securitydelay"));
+
+		System.out.println("NASdelay Delay = "
+				+ getMode(jsonArray, "nasdelay"));
+
+		System.out.println("carrierdelay Delay = "
+				+ getMode(jsonArray, "carrierdelay"));
+		
+		
+		
+		
+
 
 	}
-	
-	
-	public static double[] getFlightData(JSONArray jsonArray,String dealyAttribute) {
-		
+
+	public static double[] getFlightData(JSONArray jsonArray,
+			String dealyAttribute) {
+
 		ArrayList<Double> list = new ArrayList<Double>();
-		
+
 		for (Object object : jsonArray) {
 			JSONObject jsonObject = (JSONObject) object;
-			
+
 			String attributeValue = (String) jsonObject.get(dealyAttribute);
-			
+
 			if (attributeValue != null) {
 
-				
 				Double value = new Double(attributeValue);
 
 				if (value > 0) {
@@ -96,19 +176,18 @@ public class DataAnalysis {
 				}
 			}
 		}
-		
-		
+
 		double[] values = new double[list.size()];
-		
-		if(!list.isEmpty()){
+
+		if (!list.isEmpty()) {
 			int i = 0;
-			for(Double d:list){
+			for (Double d : list) {
 				values[i] = d.doubleValue();
 				i++;
 			}
 		}
-		
-	return values;	
+
+		return values;
 	}
 
 	public static double calculateMean(JSONArray jsonArray,
@@ -124,7 +203,6 @@ public class DataAnalysis {
 
 			if (attributeValue != null) {
 
-				
 				Double value = new Double(attributeValue);
 
 				if (value > 0) {
@@ -141,6 +219,102 @@ public class DataAnalysis {
 
 		return 0;
 	}
+
+	public static double getMedian(JSONArray jsonArray, String dealyAttribute) {
+
+		ArrayList<Double> list = new ArrayList<Double>();
+
+		for (Object object : jsonArray) {
+			JSONObject jsonObject = (JSONObject) object;
+
+			String attributeValue = (String) jsonObject.get(dealyAttribute);
+
+			if (attributeValue != null) {
+
+				Double value = new Double(attributeValue);
+
+				if (value > 0) {
+					list.add(new Double(attributeValue));
+				}
+			}
+		}
+		
+		int listSize = list.size();
+		
+		if(list.size()>0){
+		
+		return new Double(list.get(Math.round(listSize/2))).doubleValue();
+		}
+		
+		return 0;	
+
+	}
+	
+	
+	
+	
+	public static double getMode(JSONArray jsonArray, String dealyAttribute) {
+
+		ArrayList<Double> list = new ArrayList<Double>();
+
+		for (Object object : jsonArray) {
+			JSONObject jsonObject = (JSONObject) object;
+
+			String attributeValue = (String) jsonObject.get(dealyAttribute);
+
+			if (attributeValue != null) {
+
+				Double value = new Double(attributeValue);
+
+				if (value > 0) {
+					list.add(new Double(attributeValue));
+				}
+			}
+		}
+		
+		Map<Double,Integer> valueCount = new HashMap<Double,Integer>();
+		
+		
+		if(list.size() > 0){
+		
+		for(Double d:list){
+			
+			if(valueCount.get(d)!=null)
+			{
+				int count  = valueCount.get(d);
+				count++;
+				valueCount.put(d, count);
+			}else{
+				valueCount.put(d, 1);				
+			}			
+			
+		}		
+		
+	   double largeValue = 0.0;
+	   double currentKey = 0.0;
+	   
+	   
+	   for (Map.Entry<Double, Integer> entry : valueCount.entrySet()) {
+//		    String key = entry.getKey();
+//		    Object value = entry.getValue();
+		   
+		  Double key =  entry.getKey();
+		   
+		  Integer value = entry.getValue();
+		   
+		   if(value>largeValue){
+			   largeValue = value;
+			   currentKey = key;
+		   }		   
+		    
+		}
+	   
+	   return valueCount.get(currentKey);
+		}
+		
+		return 0;
+		
+   }
 
 	public static JSONArray readJsonFile(String fileName) throws Exception {
 		JSONParser parser = new JSONParser();
